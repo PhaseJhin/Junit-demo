1ad776e9088a035c2305d617bf4aeb736671d8eb
zi Jin <jinzzz@zideMacBook-Pro.local>
2018-09-10 20:04:56 -0400

    main: added ArithmeticSolver class implementation.

---
+++ src/main/java/core/ArithmeticSolver.java
@@ -0,0 +1,29 @@
+package core;
+
+public class ArithmeticSolver {
+	
+	public double add(double leftOperand, double rightOperand) {
+		
+		return leftOperand + rightOperand;
+	}
+	
+	public double subtract(double leftOperand, double rightOperand) {
+		
+		return leftOperand - rightOperand;
+	}
+	
+	public double mutiply(double leftOperand, double rightOperand) {
+	
+		return leftOperand * rightOperand;
+	}
+	
+	public double divide(double leftOperand, double rightOperand) {
+	
+		return leftOperand / rightOperand;
+	}
+	
+	public double modulus(double leftOperand, double rightOperand) {
+	
+		return leftOperand % rightOperand;
+	}
+}
--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -7,7 +7,7 @@ public class ArithmeticSolverTest extends TestCase {
 		
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4,solver.add(2,2));
+		assertEquals(4.0,solver.add(2,2));
 		
 	}
 	
@@ -15,28 +15,28 @@ public class ArithmeticSolverTest extends TestCase {
 		
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(2,solver.subtract(4,2));
+		assertEquals(2.0,solver.subtract(4,2));
 	}
 	
 	public void testMultiply() {
 		
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(16,solver.mutiply(8,2));
+		assertEquals(16.0,solver.mutiply(8,2));
 	}
 	
 	public void testDivide() {
 		
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(3,solver.divide(9,3));
+		assertEquals(3.0,solver.divide(9,3));
 	}
 		
 	public void testModulus() {
 			
 		ArithmeticSolver solver = new ArithmeticSolver();
 			
-			assertEquals(1,solver.modulus(5,2));
+			assertEquals(1.0,solver.modulus(5,2));
 		}
 
 
